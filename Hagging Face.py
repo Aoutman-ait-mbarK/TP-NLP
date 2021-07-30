202bff238978aa9a538df66dbf68e14b6d4e0e90
Aoutman-ait-mbarK <87277416+Aoutman-ait-mbarK@users.noreply.github.com>
2021-07-30 15:36:59 +0100

    Delete Hagging Face.py

--- Hagging Face.py
+++
@@ -1,8 +0,0 @@
-# Hagging Face
-#!pip install transformers
-from transformers import pipeline
-
-get_ipython().system('pip install flair')
-from transformers import AutoTokenizer
-from flair.data import Sentence
-from flair.models import SequenceTagger
